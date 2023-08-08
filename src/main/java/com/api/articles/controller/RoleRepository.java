fb6dddd3af09333dc581f363501cbc57e6b1dac1
sakshijain20 <jain.sakshi1099@gmail.com>
2023-08-08 12:18:14 +0530

    implemented authetication

---
+++ src/main/java/com/api/articles/controller/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.api.articles.controller;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.api.articles.model.ERole;
+import com.api.articles.model.Role;
+
+public interface RoleRepository extends MongoRepository<Role, String> {
+	Optional<Role> findByName(ERole name);
+
+}
---
+++ src/main/java/com/api/articles/controller/UserRepository.java
@@ -0,0 +1,15 @@
+package com.api.articles.controller;
+
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.api.articles.model.User;
+
+public interface UserRepository extends MongoRepository<User, String> {
+	  Optional<User> findByUsername(String username);
+
+	  Boolean existsByUsername(String username);
+
+	  Boolean existsByEmail(String email);
+}
